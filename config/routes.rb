24642d314a1feb17b66ce2664c7a58c835e47215
joelbeger <jbeger@digitalposition.com>
2021-02-24 12:53:08 -0500

    holy moly that was a long one

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
   ## line 5 was previously -- get "welcome/about"
   ##This allows users to visit /about rather than /welcome/about
   get 'about' => 'welcome#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,20 @@
 
 require 'random_data'
 
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
 # #1
   Post.create!(
 # #2
+    topic: topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
@@ -30,5 +39,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
